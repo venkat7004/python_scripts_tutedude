9500ec8b44d0858c59631a8aa50cd94c792c7f73
venkat7004 <102204115+venkat7004@users.noreply.github.com>
2025-07-16 06:17:34 +0530

    Add files via upload
    2nd assignment

---
+++ even_odd.py
@@ -0,0 +1,6 @@
+print("Enter an integer: ")
+num = int(input())
+if num % 2 == 0:
+    print(num," is an number")
+else:
+    print(num, "is an odd number")
---
+++ sum1-50.py
@@ -0,0 +1,8 @@
+sum = 0 
+# This code calculates the sum of numbers from 1 to 50
+
+for i in range(1,51):
+    # print(i)
+    sum = sum + i
+
+print("The sum of numbers 1 to 50 is :", sum)
