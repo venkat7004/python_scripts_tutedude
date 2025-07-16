bac5c24c3a0a33bb150d99c577cef22c03bdd348
venkat7004 <102204115+venkat7004@users.noreply.github.com>
2025-07-16 06:01:24 +0530

    Add files via upload

---
+++ BasicMath.py
@@ -0,0 +1,10 @@
+print("Enter the first number:")
+first_number = input()
+print("Enter the second number:")
+second_number = input()
+
+print("Addition:", int(first_number) + int(second_number) )
+print("Subtraction:", int(first_number) - int(second_number) )
+print("Multiplication:", int(first_number) * int(second_number) )
+print("Division:", int(first_number) / int(second_number) )
+
---
+++ greeting.py
@@ -0,0 +1,7 @@
+print("Enter your first name")
+fname=input()
+print("Enter your last name")
+lname=input()
+
+print("Hello", fname, lname, "Welcome to Python programming!")
+
